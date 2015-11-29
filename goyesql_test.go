9eca4f1f274cd3067a99c21d688216b22ae21ee8
Alexandre Leroux <alexandre@yawp.fr>
2015-11-29 15:17:54 +0800

    remove doubloon tests

--- goyesql_test.go
+++ goyesql_test.go
@@ -4,25 +4,6 @@ import (
 	"testing"
 )
 
-func TestParseFileMissing(t *testing.T) {
-	queries, err := ParseFile("tests/samples/missing.sql")
-
-	if err == nil {
-		t.Error("Parsing a missing file should return an error")
-	}
-
-	if len(queries) > 0 {
-		t.Error("Parsing a missing file should not return queries")
-	}
-}
-
-func TestParseFileExisting(t *testing.T) {
-	_, err := ParseFile("tests/samples/valid.sql")
-	if err != nil {
-		t.Errorf("Parsing a valid file should not return an error, got '%s'", err)
-	}
-}
-
 func TestMustParseFilePanic(t *testing.T) {
 	defer func() {
 		if r := recover(); r == nil {
